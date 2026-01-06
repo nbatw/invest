e6114fa13fd07c34a744603df5b690fd2c496a78
nbatw <48878313+nbatw@users.noreply.github.com>
2026-01-06 16:59:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 import streamlit as st
-
-st.title("✅ 復活成功！")
-st.write("現在你的 App 已經活過來了。")
-st.write("剛剛是因為一次裝太多工具，雲端主機跑不動超時了。")
-st.success("接下來我們可以開始把功能一個一個加回去！")
+st.title("終於活了！")
+st.balloons()
